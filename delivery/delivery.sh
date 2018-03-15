1d35359ca2ed426775998d8dbae96c0817b5f91a
Meelis Lehtmets <meelis@lehtmets.com>
2018-03-15 23:49:23 +0200

    Refactor

--- delivery/delivery.sh
+++ delivery/delivery.sh
@@ -9,23 +9,23 @@ set +a
 
 DOCKER_TAG=iglu/$PROJECT_NAME:$PROJECT_VERSION
 
-do_run(){
+build(){
     ./gradlew --no-daemon clean build
 
     cp boot/build/libs/boot.jar delivery/runner/boot.jar
     docker build -t $DOCKER_TAG delivery/runner
     rm delivery/runner/boot.jar
+}
+
+do_run(){
+    build
 
     docker stop $PROJECT_NAME || true
     docker run --rm --publish=8080:8080 --name=$PROJECT_NAME $DOCKER_TAG $1
 }
 
 do_build(){
-    ./gradlew --no-daemon clean build
-
-    cp boot/build/libs/boot.jar delivery/runner/boot.jar
-    docker build -t $DOCKER_TAG delivery/runner
-    rm delivery/runner/boot.jar
+    build
 
     docker push $DOCKER_TAG
 }
